2090f8aae7dba759dd5b0008dc3cb09089037b46
brenodinizio <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-08-30 15:40:56 -0300

    a

--- src/script.js
+++ src/script.js
@@ -13,6 +13,15 @@ var tela = document.getElementById('tela')
         var audioCorreto = document.getElementById('corretoAudio')
         var rank = document.getElementById('rank')
         var img = document.getElementById('img')
+        var buttons = document.getElementById('buttons')
+        var buttonss = document.getElementById("buttonss")
+        var buttonsss = document.getElementById('buttonsss')
+        var buttonssss = document.getElementById("buttonssss")
+        var certo = document.getElementById('certo')
+        var certoo = document.getElementById('certoo')
+        var certooo = document.getElementById("certooo")
+        var certoooo = document.getElementById('certoooo')
+        var errado = document.querySelector('div.escolhass')
 
         function playErrado(){
             audioErrado.currentTime = 0.6;
@@ -29,84 +38,190 @@ var tela = document.getElementById('tela')
             corpo.style.display = 'block'
         }
 
-        function Pergunta1(params) {
+        function Pergunta1(element, params) {
+
+            var escolhas = document.querySelectorAll('.escolhas');
+
+            for (var i = 0; i < escolhas.length; i++) {
+
+                escolhas[i].style.pointerEvents = 'none';
+
+            }
+
+            if (params == 1) {
+                playCorreto();
+                correto++;
+                buttons.style.display = 'block';
+                certo.style.background = 'green';
+            } else {
+                playErrado();
+                errado++;
+                buttons.style.display = 'block';
+                
+                certo.style.background = 'green';
+                
+                element.style.background = 'red';
+            }
+            atualizarProgresso();
+        }
+
+        function ButtonProx(){
+
+
+            Primeira.style.display = 'none'
+
+            Segunda.style.display = 'block'
+
+            buttons.style.display = 'none'
+
+            text.innerHTML = `<h2 style="font-weight: bolder;">
+            2. O que é taxonomia?</h2>`
+
+            var escolhas = document.querySelectorAll('.escolhas');
+
+        for (var i = 0; i < escolhas.length; i++) {
+            escolhas[i].style.pointerEvents = 'auto';
+            
+        }
+
+
+        }
+
+        function ButtonProxx(){
+
+
+               Segunda.style.display = 'none'
+                 Tereceira.style.display = 'block'
+                text.innerHTML = `<h2 style="font-weight: bolder;" class='textoh3'>3. Na classificação científica de animais, qual termo é 
+                 utilizado para representar uma categoria imediatamente acima da ordem e agrupa várias ordens relacionadas?</h2>`
+
+                 buttonss.style.display = 'none'
+
+            var escolhas = document.querySelectorAll('.escolhas');
+
+        for (var i = 0; i < escolhas.length; i++) {
+            escolhas[i].style.pointerEvents = 'auto';
+            
+        }
+
+
+        }
+
+        function ButtonProxxx(){
+
+
+            Tereceira.style.display = 'none'
+            Quarto.style.display = 'block'
+            text.innerHTML = `<h2 style="font-weight: bolder;" class="textoh3">4.  Qual classe de organismos foi a primeira a conquistar completamente o ambiente terrestre?</h2>`
+
+            buttonsss.style.display = 'none'
+
+         var escolhas = document.querySelectorAll('.escolhas');
+
+     for (var i = 0; i < escolhas.length; i++) {
+         escolhas[i].style.pointerEvents = 'auto';
+         
+     }
+
+
+     }
+
+
+     function ButtonProxxxx(){
+
+        Quarto.style.display = 'none'
+        res.style.display = 'block'
+        rank.style.display = 'flex'
+        res.innerHTML = `<h1 style="font-weight: bolder;">Sua Quantidade de Acertos é ${correto}/4</h1>`
+        text.innerHTML = ''
+        buttonssss.style.display = 'none'
+        progresso.style.display = 'flex'
+     var escolhas = document.querySelectorAll('.escolhas');
+
+ for (var i = 0; i < escolhas.length; i++) {
+     escolhas[i].style.pointerEvents = 'auto';
+     
+ }
+
+
+ }
+
+
+
+        function Pergunta2(element, params) {
+            
+            var escolhas = document.querySelectorAll('.escolhas');
+
+            for (var i = 0; i < escolhas.length; i++) {
+                
+                escolhas[i].style.pointerEvents = 'none';
+
+            }
+
+
             if (params == 1) {
                 playCorreto()
                 correto++
-                Primeira.style.display = 'none'
-                Segunda.style.display = 'block'
-                text.innerHTML = `<h2 style="font-weight: bolder;">
-                2. O que é taxonomia?</h2>`
+                buttonss.style.display = 'block';
+                certoo.style.background = 'green'
+
+              
+
             }else{
                 playErrado()
                 errado++
-                Primeira.style.display = 'none'
-                Segunda.style.display = 'block'
-                text.innerHTML = `<h2 style="font-weight: bolder;">
-                2. O que é taxonomia?</h2>`
+                element.style.background = 'red'
+                certoo.style.background = 'green'
+                buttonss.style.display = 'block';
             }
             atualizarProgresso()
         }
 
-        function Pergunta2(params) {
+
+
+        function Pergunta3(element, params) {
+
+            var escolhas = document.querySelectorAll('.escolhas');
+
+            for (var i = 0; i < escolhas.length; i++) {
+                
+                escolhas[i].style.pointerEvents = 'none';
+
+            }
+
             if (params == 1) {
+
                 playCorreto()
                 correto++
-                Segunda.style.display = 'none'
-                Tereceira.style.display = 'block'
-                text.innerHTML = `<h2 style="font-weight: bolder;">3. Na classificação científica de animais, qual termo é 
-                utilizado para representar uma categoria imediatamente acima da ordem e agrupa várias ordens relacionadas?</h2>`
-
+                certooo.style.background = 'green'
+                buttonsss.style.display = 'block';
             }else{
+
                 playErrado()
                 errado++
-                Segunda.style.display = 'none'
-                Tereceira.style.display = 'block'
-                text.innerHTML = `<h2 style="font-weight: bolder;">3. Na classificação científica de animais, qual termo é utilizado para representar uma categoria imediatamente 
-                acima da ordem e agrupa várias ordens relacionadas?</h2>`
+                element.style.background = 'red'
+                certooo.style.background = 'green'
+                buttonsss.style.display = 'block';
             }
             atualizarProgresso()
         }
 
-        function Pergunta3(params) {
+        function Pergunta4(element, params) {
             if (params == 1) {
                 playCorreto()
                 correto++
-                Tereceira.style.display = 'none'
-                Quarto.style.display = 'block'
-                text.innerHTML = `<h2 style="font-weight: bolder;">4.  Qual classe de organismos foi a primeira a conquistar completamente o ambiente terrestre?</h2>`
+                certoooo.style.background = 'green'
+                buttonssss.style.display = 'block';
 
             }else{
                 playErrado()
                 errado++
-                Tereceira.style.display = 'none'
-                Quarto.style.display = 'block'
-                text.innerHTML = `<h2 style="font-weight: bolder;">4. Qual classe de organismos foi a primeira a conquistar completamente o ambiente terrestre?</h2>`
+                element.style.background = 'red'
+                certoooo.style.background = 'green'
+                buttonssss.style.display = 'block';
             }
-            atualizarProgresso()
-        }
-
-        function Pergunta4(params) {
-            if (params == 1) {
-                playCorreto()
-                correto++
-                Quarto.style.display = 'none'
-                res.style.display = 'block'
-                rank.style.display = 'flex'
-                res.innerHTML = `<h1 style="font-weight: bolder;">Sua Quantidade de Acertos é ${correto}/4</h1>`
-                text.innerHTML = ''
-
-            }else{
-                playErrado()
-                errado++
-                res.innerHTML = `<h1 style="font-weight: bolder;">Sua Quantidade de Acertos é ${correto}/4</h1>`
-                Quarto.style.display = 'none'
-                res.style.display = 'block'
-                rank.style.display = 'flex'
-                text.innerHTML = ''
-            }
-            corpo.style.width = '60%'
-            progresso.style.display = 'flex'
+            
+            
 
             if(correto < 1){
                 img.setAttribute('src', '/images/rank1.png');
@@ -125,6 +240,7 @@ var tela = document.getElementById('tela')
 
 
         function atualizarProgresso() {
+
             var progressSegmentOne = document.getElementById("correto");
             var progressSegmentTwo = document.getElementById("errado");
     
@@ -136,4 +252,5 @@ var tela = document.getElementById('tela')
     
             progressSegmentTwo.style.width = widthIncrementErrado + "%";
             progressSegmentTwo.setAttribute("aria-valuenow", widthIncrementErrado);
+
         }
